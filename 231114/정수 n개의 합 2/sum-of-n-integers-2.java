30ff61762084d6391d01a7c0a8fbb861c2f2b794
Taek Seung Lee <58359383+dlxortmd987@users.noreply.github.com>
2023-11-14 21:08:56 +0900

    Title: 정수 n개의 합 2, Time: 910ms, Memory: 36MB, Status: Passed - CodeTree

---
+++ 231114/정수 n개의 합 2/sum-of-n-integers-2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		for (int i = 1; i < n; i++) {
+			arr[i] += arr[i - 1];
+		}
+
+		int res = - Integer.MAX_VALUE;
+		for (int i = 0; i < n - k; i++) {
+			res = Math.max(arr[i + k] - arr[i], res);
+		}
+
+		System.out.println(res);
+	}
+}
