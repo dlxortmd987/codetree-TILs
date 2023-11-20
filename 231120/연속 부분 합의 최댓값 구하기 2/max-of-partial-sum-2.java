a2c9b1460954d3b3358d94e6cc9a9abc82bd9a9b
Taek Seung Lee <58359383+dlxortmd987@users.noreply.github.com>
2023-11-20 18:09:36 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 795ms, Memory: 33MB, Status: Passed - CodeTree

--- 231120/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
+++ 231120/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
@@ -8,22 +8,18 @@ public class Main {
 
         int[] arr = new int[n];
 
-        int res = -1001;
+        int res = Integer.MIN_VALUE;
         int temp = 0;
-        int max = -1001;
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
-            max = Math.max(max, arr[i]);
-            if (temp + arr[i] < temp) {
-                temp = 0;
-            } else if (temp + arr[i] > temp) {
+            
+            if (temp < 0) {
+                temp = arr[i];
+            } else {
                 temp += arr[i];
-                res = temp;
             }
-        }
 
-        if (res == -1001) {
-            res = max;
+            res = Math.max(res, temp);
         }
 
         System.out.println(res);
