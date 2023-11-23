b101c525826c58adb91ee874bcf08b74b8aa7afb
Taek Seung Lee <58359383+dlxortmd987@users.noreply.github.com>
2023-11-23 14:43:17 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 841ms, Memory: 34MB, Status: Passed - CodeTree

--- 231123/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
+++ 231123/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
@@ -1,20 +1,63 @@
 import java.util.*;
 
 public class Main {
+
+    public static int find(List<Integer> b, int v) {
+        int s = 0;
+        int e = b.size() - 1;
+
+        while (s <= e) {
+            int mid = (s + e) / 2;
+
+            if (b.get(mid) < v) {
+                s = mid + 1;
+            } else if (b.get(mid) > v) {
+                e = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
-        int[] arr = new int[n];
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+
+        
+        for (int i = 0; i < n; i++) {
+            b.add(sc.nextInt());
+        }
+
+        Collections.sort(b);
+
+        for (int i = 1; i <= 2*n; i++) {
+            if (find(b, i) == -1) {
+                a.add(i);
+            }
+        }
+
+        Collections.sort(a);
 
         int res = 0;
+        int idx = 0;
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-
-            if (arr[i] < 2 * n) {
-                res++;
+            if (idx >= a.size())
+                break;
+            while (a.get(idx) < b.get(i)) {
+                if (idx + 1 < a.size()) {
+                    idx++;
+                } else
+                    break;
             }
+
+            res++;
+            idx++;
         }
 
         System.out.println(res);
